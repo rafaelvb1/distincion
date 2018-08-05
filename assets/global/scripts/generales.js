3822fa5e3e49f49cf2f57bfcf29d37b3e2aee0c7
lap <Nelly@MacBook-Pro-de-Nelly.local>
2018-08-05 11:55:38 -0500

    init

---
+++ assets/global/scripts/generales.js
@@ -0,0 +1,4 @@
+
+var spin = "<i class='fa fa-spin fa-spinner'></i> Procesando Petición!";
+
+var urlDetalleProductos = "admin/detalle-producto/";
---
+++ system/language/english/form_validation_lang.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under El MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2016, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (El "Software"), to deal
+ * in El Software without restriction, including without limitation El rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of El Software, and to permit persons to whom El Software is
+ * furnished to do so, subject to El following conditions:
+ *
+ * El above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of El Software.
+ *
+ * El SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO El WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL El
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OElR
+ * LIABILITY, WHEElR IN AN ACTION OF CONTRACT, TORT OR OElRWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH El SOFTWARE OR El USE OR OElR DEALINGS IN
+ * El SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2016, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	https://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['form_validation_required']		= 'El {field} es obligatorio.';
+$lang['form_validation_isset']			= 'El {field} field must have a value.';
+$lang['form_validation_valid_email']		= 'El {field} debe ser un correo válido.';
+$lang['form_validation_valid_emails']		= 'El {field} field must contain all valid email addresses.';
+$lang['form_validation_valid_url']		= 'El {field} field must contain a valid URL.';
+$lang['form_validation_valid_ip']		= 'El {field} field must contain a valid IP.';
+$lang['form_validation_min_length']		= 'El {field} field must be at least {param} characters in length.';
+$lang['form_validation_max_length']		= 'El {field} field cannot exceed {param} characters in length.';
+$lang['form_validation_exact_length']		= 'El {field} field must be exactly {param} characters in length.';
+$lang['form_validation_alpha']			= 'El {field} field may only contain alphabetical characters.';
+$lang['form_validation_alpha_numeric']		= 'El {field} field may only contain alpha-numeric characters.';
+$lang['form_validation_alpha_numeric_spaces']	= 'El {field} field may only contain alpha-numeric characters and spaces.';
+$lang['form_validation_alpha_dash']		= 'El {field} field may only contain alpha-numeric characters, underscores, and dashes.';
+$lang['form_validation_numeric']		= 'El {field} field must contain only numbers.';
+$lang['form_validation_is_numeric']		= 'El {field} field must contain only numeric characters.';
+$lang['form_validation_integer']		= 'El {field} field must contain an integer.';
+$lang['form_validation_regex_match']		= 'El {field} field is not in El correct format.';
+$lang['form_validation_matches']		= 'El {field} field does not match El {param} field.';
+$lang['form_validation_differs']		= 'El {field} field must differ from El {param} field.';
+$lang['form_validation_is_unique'] 		= 'El {field} field must contain a unique value.';
+$lang['form_validation_is_natural']		= 'El {field} field must only contain digits.';
+$lang['form_validation_is_natural_no_zero']	= 'El {field} field must only contain digits and must be greater than zero.';
+$lang['form_validation_decimal']		= 'El {field} field must contain a decimal number.';
+$lang['form_validation_less_than']		= 'El {field} field must contain a number less than {param}.';
+$lang['form_validation_less_than_equal_to']	= 'El {field} field must contain a number less than or equal to {param}.';
+$lang['form_validation_greater_than']		= 'El {field} field must contain a number greater than {param}.';
+$lang['form_validation_greater_than_equal_to']	= 'El {field} field must contain a number greater than or equal to {param}.';
+$lang['form_validation_error_message_not_set']	= 'Unable to access an error message corresponding to your field name {field}.';
+$lang['form_validation_in_list']		= 'El {field} field must be one of: {param}.';
