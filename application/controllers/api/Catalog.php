96c73177c6403b0ab53dcc2c29b062375cae29d4
Rafael Pani <rafaelpani@MacBook-Pro-de-Rafael.local>
2018-08-11 20:41:55 -0500

    test1

--- application/controllers/api/Catalog.php
+++ application/controllers/api/Catalog.php
@@ -1,6 +1,7 @@
 	<?php
     ini_set("display_errors", "0");
 	defined('BASEPATH') OR exit('No direct script access allowed');
+	
 
 	// This can be removed if you use __autoload() in config.php OR use Modular Extensions
 	require APPPATH . '/libraries/REST_Controller.php';
